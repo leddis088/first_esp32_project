4fa03f818264590012af457839d0f81228cf1051
Leddis08 <65092977+leddis088@users.noreply.github.com>
2024-04-21 22:45:57 +0200

    added dc-messages && cleaned the code up

--- esp32.cpp
+++ esp32.cpp
@@ -11,7 +11,7 @@
 const char* ssid = "";
 const char* password = "";
 
-const char* apiToken = ";
+const char* apiToken = "";
 const char* userToken = "";
 const char* pushoverApiEndpoint = "https://api.pushover.net/1/messages.json";
 const char *PUSHOVER_ROOT_CA = "-----BEGIN CERTIFICATE-----\n"
@@ -36,6 +36,34 @@ const char *PUSHOVER_ROOT_CA = "-----BEGIN CERTIFICATE-----\n"
                   "pLiaWN0bfVKfjllDiIGknibVb63dDcY3fe0Dkhvld1927jyNxF1WW6LZZm6zNTfl\n"
                   "MrY=\n"
                   "-----END CERTIFICATE-----\n";
+
+const String discord_webhook = "";
+const String discord_tts = "false";
+const char* DISCORD_CERT = R"(
+-----BEGIN CERTIFICATE-----
+MIIDzTCCArWgAwIBAgIQCjeHZF5ftIwiTv0b7RQMPDANBgkqhkiG9w0BAQsFADBa
+MQswCQYDVQQGEwJJRTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJl
+clRydXN0MSIwIAYDVQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTIw
+MDEyNzEyNDgwOFoXDTI0MTIzMTIzNTk1OVowSjELMAkGA1UEBhMCVVMxGTAXBgNV
+BAoTEENsb3VkZmxhcmUsIEluYy4xIDAeBgNVBAMTF0Nsb3VkZmxhcmUgSW5jIEVD
+QyBDQS0zMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEua1NZpkUC0bsH4HRKlAe
+nQMVLzQSfS2WuIg4m4Vfj7+7Te9hRsTJc9QkT+DuHM5ss1FxL2ruTAUJd9NyYqSb
+16OCAWgwggFkMB0GA1UdDgQWBBSlzjfq67B1DpRniLRF+tkkEIeWHzAfBgNVHSME
+GDAWgBTlnVkwgkdYzKz6CFQ2hns6tQRN8DAOBgNVHQ8BAf8EBAMCAYYwHQYDVR0l
+BBYwFAYIKwYBBQUHAwEGCCsGAQUFBwMCMBIGA1UdEwEB/wQIMAYBAf8CAQAwNAYI
+KwYBBQUHAQEEKDAmMCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2VydC5j
+b20wOgYDVR0fBDMwMTAvoC2gK4YpaHR0cDovL2NybDMuZGlnaWNlcnQuY29tL09t
+bmlyb290MjAyNS5jcmwwbQYDVR0gBGYwZDA3BglghkgBhv1sAQEwKjAoBggrBgEF
+BQcCARYcaHR0cHM6Ly93d3cuZGlnaWNlcnQuY29tL0NQUzALBglghkgBhv1sAQIw
+CAYGZ4EMAQIBMAgGBmeBDAECAjAIBgZngQwBAgMwDQYJKoZIhvcNAQELBQADggEB
+AAUkHd0bsCrrmNaF4zlNXmtXnYJX/OvoMaJXkGUFvhZEOFp3ArnPEELG4ZKk40Un
++ABHLGioVplTVI+tnkDB0A+21w0LOEhsUCxJkAZbZB2LzEgwLt4I4ptJIsCSDBFe
+lpKU1fwg3FZs5ZKTv3ocwDfjhUkV+ivhdDkYD7fa86JXWGBPzI6UAPxGezQxPk1H
+goE6y/SJXQ7vTQ1unBuCJN0yJV0ReFEQPaA1IwQvZW+cwdFD19Ae8zFnWSfda9J1
+CZMRJCQUzym+5iPDuI9yP+kHyCREU3qzuWFloUwOxkgAyXVjBYdwRVKD05WdRerw
+6DEdfgkfCv4+3ao8XnTSrLE=
+-----END CERTIFICATE-----
+)";
 
 const int buttonPin = 23;
 const int swPinOn = 25;
@@ -73,31 +101,36 @@ void setup() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting to WiFi...");
     lcd.clear();
     lcd.setCursor(0, 1);
     lcd.print("Connecting to WiFi..");
+    Serial.println("Connecting to WiFi...");
+    sendDiscordMessage("Connecting to WiFi...");
     delay(1000);
   }
-  Serial.println("Connected to WiFi");
   lcd.clear();
   lcd.setCursor(1, 1);
   lcd.print("Connected to WiFi");
+  Serial.println("Connected to WiFi");
+  sendDiscordMessage("Connected to WiFi");
   delay(1000);
-  Serial.println("TEST BEGIN");
   lcd.clear();
   lcd.setCursor(5, 1);
   lcd.print("TEST BEGIN");
+  Serial.println("TEST BEGIN");
+  sendDiscordMessage("TEST BEGIN");
   delay(750);
   lcd.clear();
   lcd.setCursor(8,1);
   lcd.print("TEST");
-  Serial.println("BEEP");
   lcd.setCursor(8,2);
   lcd.print("BEEP");
+  Serial.println("BEEP");
+  sendDiscordMessage("BEEP");
   for (int thisNote = 0; thisNote < 1; thisNote++) {
     int noteDuration = 1000 / noteDurations[thisNote];
     tone(BUZZZER_PIN, melody[thisNote], noteDuration);
+    Serial.println("PLAYING TONE");
     int pauseBetweenNotes = noteDuration * 1.30;
     delay(pauseBetweenNotes);
     noTone(BUZZZER_PIN);
@@ -109,7 +142,6 @@ void setup() {
     lcd.print("TEST");
     lcd.setCursor(0,2);
     lcd.print("SENDING PUSH MESSEGE");
-    delay(500);
     StaticJsonDocument<512> notification; 
       notification["token"] = apiToken; //required
       notification["user"] = userToken; //required
@@ -118,6 +150,7 @@ void setup() {
       notification["priority"] = "0"; //optional
       notification["sound"] = "cosmic"; //optional
     String jsonStringNotification;
+    Serial.println("message saved to JSON");
     serializeJson(notification, jsonStringNotification);
     HTTPClient https;
     https.begin(pushoverApiEndpoint);
@@ -133,44 +166,61 @@ void setup() {
       }
     https.end();
   }
+  delay(600);
+  lcd.clear();
+  lcd.setCursor(8,1);
+  lcd.print("TEST");
+  lcd.setCursor(1,2);
+  lcd.print("SENDING DC MESSEGE");
+  sendDiscordMessage("Hello from the emergency stop");
+  delay(300);
   lcd.clear();
   lcd.setCursor(8,1);
   lcd.print("TEST");
   lcd.setCursor(3,2);
   lcd.print("RED LED BLINK");
-  delay(100);
   Serial.println("RED LED Blink");
+  sendDiscordMessage("RED LED Blink");
   delay(200);
   digitalWrite(2, HIGH);
+  Serial.println("RED LED status HIGH");
   delay(200);
   digitalWrite(2, LOW);
+  Serial.println("RED LED status LOW");
   delay(500);
-  Serial.println("YELLOW LED BLINK");
   lcd.clear();
   lcd.setCursor(8,1);
   lcd.print("TEST");
   lcd.setCursor(1,2);
   lcd.print("YELLOW LED BLINK");
+  sendDiscordMessage("YELLOW LED BLINK");
+  Serial.println("YELLOW LED BLINK");
   delay(200);
   digitalWrite(yellowLED, HIGH);
+  Serial.println("YELLOW LED status HIGH");
   delay(200);
   digitalWrite(yellowLED, LOW);
+  Serial.println("YELLOW LED status LOW");
   delay(500);
-  Serial.println("GREEN LED BLINK");
   lcd.clear();
   lcd.setCursor(8,1);
   lcd.print("TEST");
   lcd.setCursor(2,2);
   lcd.print("GREEN LED BLINK");
+  Serial.println("GREEN LED BLINK");
+  sendDiscordMessage("GREEN LED BLINK");
   delay(200);
   digitalWrite(greenLED, HIGH);
+  Serial.println("GREEN LED status HIGH");
   delay(200);
   digitalWrite(greenLED, LOW);
+  Serial.println("GREEN LED status HIGH");
   delay(500);
-  Serial.println("TEST END");
   lcd.clear();
   lcd.setCursor(6,1);
   lcd.print("TEST END");
+  Serial.println("TEST END");
+  sendDiscordMessage("TEST END");
   delay(750);
   if (digitalRead(swPinOn) == LOW) {
     startup();
@@ -193,22 +243,31 @@ void loop() {
         while (led < 4){
           delay(200);
           digitalWrite(redLED, HIGH);
+          Serial.println("RED LED status HIGH");
           digitalWrite(yellowLED, HIGH);
+          Serial.println("YELLOW LED status HIGH");
           digitalWrite(greenLED, HIGH);
+          Serial.println("GREEN LED status HIGH");
           delay(200);
           digitalWrite(redLED, LOW);
+          Serial.println("RED LED status LOW");
           digitalWrite(yellowLED, LOW);
+          Serial.println("YELLOW LED status LOW");
           digitalWrite(greenLED, LOW);
+          Serial.println("GREEN LED status LOW");
           led ++;
         }
         led = 0;
         digitalWrite(redLED, LOW);
+        Serial.println("RED LED status LOW");
         digitalWrite(greenLED, HIGH);
-        Serial.println("ALARM HAS BEEN RESET!");
+        Serial.println("GREEN LED status HIGH");
         lcd.clear();
         lcd.print("ALARM OS v.0.5");
         lcd.setCursor(0,1);
         lcd.print("ALARM HAS BEEN RESET");
+        Serial.println("ALARM HAS BEEN RESET");
+        sendDiscordMessage("ALARM HAS BEEN RESET");
         if (WiFi.status() == WL_CONNECTED) {
           StaticJsonDocument<512> notification; 
           notification["token"] = apiToken; //required
@@ -216,6 +275,7 @@ void loop() {
           notification["message"] = "The Alarm is reseted!"; //required
           notification["title"] = "RESET"; //optional
           String jsonStringNotification;
+          Serial.println("message saved to JSON");
           serializeJson(notification, jsonStringNotification);
           HTTPClient https;
           https.begin(pushoverApiEndpoint);
@@ -240,12 +300,14 @@ void loop() {
       }
       if (digitalRead(yellowLED) == HIGH) {
         digitalWrite(yellowLED, LOW);
+        Serial.println("YELLOW LED status LOW");
       }
-      Serial.println("System is idleing");
       lcd.clear();
       lcd.print("ALARM OS v.0.5");
       lcd.setCursor(1,1);
       lcd.print("SYSTEM IS IDLEING");
+      Serial.println("System is idleing");
+      sendDiscordMessage("System is idleing");
       if (digitalRead(testPin) == HIGH) {
         delay(150); 
         if (digitalRead(testPin) == HIGH) {
@@ -254,6 +316,7 @@ void loop() {
           lcd.setCursor(4,1);
           lcd.print("TEST ALRM");
           Serial.println("TEST ALARM");
+          sendDiscordMessage("TEST ALARM");
           if (WiFi.status() == WL_CONNECTED) {
             StaticJsonDocument<512> notification; 
             notification["token"] = apiToken; //required
@@ -261,6 +324,7 @@ void loop() {
             notification["message"] = "TEST ALARM MESSAGE"; //required
             notification["title"] = "TEST ALARM"; //optional
             String jsonStringNotification;
+            Serial.println("message saved to JSON");
             serializeJson(notification, jsonStringNotification);
             HTTPClient https;
             https.begin(pushoverApiEndpoint);
@@ -279,6 +343,7 @@ void loop() {
           for (int thisNote = 0; thisNote < 14; thisNote++) {
             int noteDuration = 1000 / noteDurations[thisNote];
             tone(BUZZZER_PIN, melody[thisNote], noteDuration);
+            Serial.println("PLAYING TONE");
             int pauseBetweenNotes = noteDuration * 1.30;
             delay(pauseBetweenNotes);
             noTone(BUZZZER_PIN);
@@ -291,16 +356,25 @@ void loop() {
     if (swOnOff == 1) {
       lcd.clear();
       lcd.print("SHOUTTING DOWN OS...");
+      Serial.println("SHOUTTING DOWN OS...");
+      sendDiscordMessage("SHOUTTING DOWN OS...");
       delay(2100);
       lcd.clear();
       lcd.print("SHOUTTED DOWN OS");
+      Serial.println("SHOUTTED DOWN OS");
+      sendDiscordMessage("SHOUTTED DOWN OS");
       delay(750);
       lcd.clear();
       lcd.setCursor(1,1);
       lcd.print("ALARM IS NOT READY");
+      Serial.println("ALARM IS NOT READY");
+      sendDiscordMessage("ALARM IS NOT READY");
       digitalWrite(redLED, LOW);
+      Serial.println("RED LED status LOW");
       digitalWrite(yellowLED, LOW);
+      Serial.println("YELLOW LED status LOW");
       digitalWrite(greenLED, LOW);
+      Serial.println("GREEN LED status LOW");
       swOnOff = 0;
       alarmCounter = 0;
       if (WiFi.status() == WL_CONNECTED) {
@@ -310,6 +384,7 @@ void loop() {
           notification["message"] = "The Alarm has been turned off!"; //required
           notification["title"] = "SHOUTDOWN"; //optional
           String jsonStringNotification;
+          Serial.println("message saved to JSON");
           serializeJson(notification, jsonStringNotification);
           HTTPClient https;
           https.begin(pushoverApiEndpoint);
@@ -329,6 +404,8 @@ void loop() {
       lcd.clear();
       lcd.setCursor(1,1);
       lcd.print("ALARM IS NOT READY");
+      Serial.println("ALARM IS NOT READY");
+      sendDiscordMessage("ALARM IS NOT READY");
       swOnOff = 0;
     }
   }
@@ -340,12 +417,18 @@ void loop() {
 void startup() {
   lcd.clear();
   lcd.print("LOADING OS...");
+  Serial.println("LOADING OS...");
+  sendDiscordMessage("LOADING OS...");
   delay(1500);
   lcd.setCursor(0,1);
   lcd.print("ALARM OS v.0.5");
+  Serial.println("ALARM OS v.0.5");
+  sendDiscordMessage("ALARM OS v.0.5");
   delay(700);
   lcd.setCursor(0,2);
   lcd.print("LOADING SOUNDFILES..");
+  Serial.println("LOADING SOUNDFILES..");
+  sendDiscordMessage("LOADING SOUNDFILES..");
   delay(1500);
   lcd.clear();
   lcd.print("LOADING OS...");
@@ -354,11 +437,16 @@ void startup() {
   lcd.setCursor(0,2);
   delay(1500);
   lcd.print("SOUNDFILES LOADED");
+  Serial.println("SOUNDFILES LOADED");
+  sendDiscordMessage("SOUNDFILES LOADED");
   delay(750);
   lcd.clear();
   lcd.setCursor(3,1);
   lcd.print("ALARM IS READY");
+  Serial.println("ALARM IS READY");
+  sendDiscordMessage("ALARM IS READY");
   digitalWrite(greenLED, HIGH);
+  Serial.println("GREEN LED status HIGH");
   swOnOff = 1;
   if (WiFi.status() == WL_CONNECTED) {
     StaticJsonDocument<512> notification; 
@@ -367,6 +455,7 @@ void startup() {
     notification["message"] = "The Alarm has started and is ready"; //required
     notification["title"] = "STARTUP"; //optional
     String jsonStringNotification;
+    Serial.println("message saved to JSON");
     serializeJson(notification, jsonStringNotification);
     HTTPClient https;
     https.begin(pushoverApiEndpoint);
@@ -391,11 +480,12 @@ void startup() {
 void alarm() {
   alarmCounter++;
   if (alarmCounter == 1) {
-    Serial.println("WARNING!");
     lcd.clear();
     lcd.print("ALARM OS v.0.5");
     lcd.setCursor(6,1);
     lcd.print("WARNING");
+    Serial.println("WARNING");
+    sendDiscordMessage("WARNING");
     if (WiFi.status() == WL_CONNECTED) {
       StaticJsonDocument<512> notification; 
       notification["token"] = apiToken; //required
@@ -405,6 +495,7 @@ void alarm() {
       notification["priority"] = "1"; //optional
       notification["sound"] = "siren"; //optional
       String jsonStringNotification;
+      Serial.println("message saved to JSON");
       serializeJson(notification, jsonStringNotification);
       HTTPClient https;
       https.begin(pushoverApiEndpoint);
@@ -424,11 +515,12 @@ void alarm() {
     digitalWrite(yellowLED, HIGH);
     delay(10000);
   } else if (alarmCounter == 2) {
-    Serial.println("SECOND WARNING!");
     lcd.clear();
     lcd.print("ALARM OS v.0.5");
     lcd.setCursor(3,1);
     lcd.print("SECOND WARNING");
+    Serial.println("SECOND WARNING!");
+    sendDiscordMessage("SECOND WARNING");
     if (WiFi.status() == WL_CONNECTED) {
       StaticJsonDocument<512> notification; 
       notification["token"] = apiToken; //required
@@ -438,6 +530,7 @@ void alarm() {
       notification["priority"] = "1"; //optional
       notification["sound"] = "siren"; //optional
       String jsonStringNotification;
+      Serial.println("message saved to JSON");
       serializeJson(notification, jsonStringNotification);
       HTTPClient https;
       https.begin(pushoverApiEndpoint);
@@ -464,12 +557,6 @@ void alarm() {
     delay(20000);
   } else {
     Serial.println("ALARM!");
-    digitalWrite(yellowLED, LOW);
-    digitalWrite(redLED, HIGH);
-    lcd.clear();
-    lcd.print("ALARM OS v.0.5");
-    lcd.setCursor(7,1);
-    lcd.print("ALARM");
     if (alarmCounter == 3) {
       counter = 0;
       if (WiFi.status() == WL_CONNECTED) {
@@ -481,6 +568,7 @@ void alarm() {
         notification["priority"] = "1"; //optional
         notification["sound"] = "siren"; //optional
         String jsonStringNotification;
+        Serial.println("message saved to JSON");
         serializeJson(notification, jsonStringNotification);
         HTTPClient https;
         https.begin(pushoverApiEndpoint);
@@ -496,13 +584,57 @@ void alarm() {
         }
       https.end();
       }
+      digitalWrite(yellowLED, LOW);
+      digitalWrite(redLED, HIGH);
+      lcd.clear();
+      lcd.print("ALARM OS v.0.5");
+      lcd.setCursor(7,1);
+      lcd.print("ALARM");
+      sendDiscordMessage("ALARM");
     }
     for (int thisNote = 0; thisNote < 14; thisNote++) {
       int noteDuration = 1000 / noteDurations[thisNote];
       tone(BUZZZER_PIN, melody[thisNote], noteDuration);
+      Serial.println("PLAYING TONE");
       int pauseBetweenNotes = noteDuration * 1.30;
       delay(pauseBetweenNotes);
       noTone(BUZZZER_PIN);
     }
   }
+}
+
+/**
+ * Discord integration
+ */
+ void sendDiscord(String content, String embedJson) {
+  WiFiClientSecure *client = new WiFiClientSecure;
+  if (client) {
+    client -> setCACert(DISCORD_CERT);
+    {
+      HTTPClient https;
+      if (https.begin(*client, discord_webhook)) {
+        https.addHeader("Content-Type", "application/json");
+        String jsonPayload = "{\"content\":\"" + content + "\",\"tts\":" + discord_tts + ",\"embeds\": [" + embedJson + "]}";
+        int httpCode = https.POST(jsonPayload);
+        if (httpCode > 0) {
+          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+            String payload = https.getString();
+            Serial.print("HTTP Response: ");
+            Serial.println(payload);
+          }
+        } else {
+          Serial.print("HTTP Post failed: ");
+          Serial.println(https.errorToString(httpCode).c_str());
+        }
+        https.end();
+      }
+    }
+    delete client;
+  }
+}
+void sendDiscordMessage(String content) {
+  sendDiscord(content, "");
+}
+void sendDiscordEmbeds(String embeds) {
+  sendDiscord("", embeds);
 }
